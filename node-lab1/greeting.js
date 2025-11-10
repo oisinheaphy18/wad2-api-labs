ece428ec38c16382bf0d493782ce0de2e4d5a19e
Oisin Heaphy <YOUR_EMAIL@EXAMPLE.COM>
2025-11-10 21:31:57 +0000

    Completed node lab 1!

--- node-lab1/greeting.js
+++ node-lab1/greeting.js
@@ -1,6 +1,9 @@
 const greeting = {
-    en: "Hello World!",
-    fr: "Bonjour!"
+  en: "Hello World!",
+  fr: "Bonjour!",
+  ga: "Dia duit!",
+  es: "¡Hola!",
+  de: "Hallo!",
 };
 
 export default greeting;
